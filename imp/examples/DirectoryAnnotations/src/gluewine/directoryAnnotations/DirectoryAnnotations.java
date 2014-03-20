37912084cb95039e0c72253003d155ac267e217c
GondaJanssen <gonda.janssen@fks.be>
2014-03-20 11:25:14 +0100

    Example Directory annotation added

---
+++ imp/examples/DirectoryAnnotations/src/gluewine/directoryAnnotations/DirectoryAnnotations.java
@@ -0,0 +1,35 @@
+package gluewine.directoryAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.gluewine.console.CLICommand;
+import org.gluewine.console.CommandContext;
+import org.gluewine.console.CommandProvider;
+import org.gluewine.core.Glue;
+
+
+public class DirectoryAnnotations implements CommandProvider
+{
+	@Glue(properties = "test.properties")
+    private Properties props;
+
+    public void _testDA(CommandContext cc)
+    {
+    	cc.println("This is a test-command");
+        
+    }   
+
+
+    @Override
+    public List<CLICommand> getCommands()
+    {
+        List<CLICommand> l = new ArrayList<>();
+
+        CLICommand cmd = new CLICommand("testDA", "Test command for gluewine with using directory annotations!");
+        l.add(cmd);
+
+        return l;
+    }
+}
