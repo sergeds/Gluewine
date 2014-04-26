eef25e1b74fbcc012febabcb92b9bed9100c55e4
Serge de Schaetzen <serge.de.schaetzen@fks.be>
2014-04-26 11:32:54 +0200

    Updated the manifests for issue #4

---
+++ imp/src/java/org/gluewine/core/ContextInitializer.java
@@ -0,0 +1,42 @@
+/**************************************************************************
+ *
+ * Gluewine Core Module
+ *
+ * Copyright (C) 2013 FKS bvba               http://www.fks.be/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ***************************************************************************/
+package org.gluewine.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Methods annotated with this annotation will be invoked without setting the firstInChain flag to false.
+ * Remark: if the firstInChain flag is set to false, encountering this method in the stack will NOT reset the flag.
+ *
+ * AspectProviders annotated with this annotation will be invoked when a ContextInitializer method is encountered.
+ * (and only they will be invoked)
+ *
+ * @author fks/Serge de Schaetzen
+ *
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface ContextInitializer
+{
+}
