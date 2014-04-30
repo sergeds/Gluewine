63b2ce53626cb2864197c6ad831bc1247618fa15
GondaJanssen <gonda.janssen@fks.be>
2014-04-30 15:03:55 +0200

    Overview of contacts added

---
+++ imp/examples/GluewineExampleApp/src/gluewine/rest/OverviewContacts.java
@@ -0,0 +1,69 @@
+package gluewine.rest;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.gluewine.core.Glue;
+import org.gluewine.jetty.GluewineServlet;
+import org.gluewine.persistence_jpa_hibernate.HibernateSessionProvider;
+
+import gluewine.entities.Contact;
+
+
+
+public class OverviewContacts extends GluewineServlet {
+
+	@Override
+	public String getContextPath() {
+		return "contacts";
+	}
+	
+	@Glue
+    private HibernateSessionProvider provider;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		List<Contact> contacts = provider.getSession().getAll(Contact.class);
+    	
+    	resp.setContentType("text/html");
+		resp.getWriter().write(""
+				+ "<html>"
+        			+ "<body>"
+        				+ "<h1>Contacts</h1>"
+        					+ "<table border=\"1\">"
+	        					+ "<tr>"
+									+ "<th> Id </th>"
+									+ "<th> Firstname </th>"
+									+ "<th> Lastname </th>"
+									+ "<th> Email </th>"
+									+ "<th> Phone number </th>"
+								+ "</tr>");
+		
+		for (Contact contact : contacts) {
+    		resp.getWriter().write(""
+    							+ "<tr>"
+    								+ "<td> " + contact.getId() + "</td>"
+    								+ "<td> " + contact.getFirstname() + "</td>"
+    								+ "<td> " + contact.getLastname() + "</td>"
+    								+ "<td> " + contact.getEmail() + "</td>"
+    								+ "<td> " + contact.getPhoneNumber() + "</td>"    								    							
+    							+ "</tr>");  
+		}
+		
+		resp.getWriter().write(""
+						+ "</table>"
+					+ "</body>"
+        		+ "</html>");
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
+    {
+        
+    }
+	
+
+}
