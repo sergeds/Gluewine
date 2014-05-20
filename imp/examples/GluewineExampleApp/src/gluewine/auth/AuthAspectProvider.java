3219940b2f73e69e22b4d079380931311ead6c11
GondaJanssen <gonda.janssen@fks.be>
2014-05-20 09:26:29 +0200

    Removed style.css & working on aspectprovider

--- imp/examples/GluewineExampleApp/src/gluewine/auth/AuthAspectProvider.java
+++ imp/examples/GluewineExampleApp/src/gluewine/auth/AuthAspectProvider.java
@@ -20,37 +20,31 @@ import gluewine.entities.User;
 
 public class AuthAspectProvider implements AspectProvider{
 
-	/*
-	 * When the users logs in, we check in the beforeInvocation-method if he is an admin or a simple user.
-	 * Based on this, we redirect him to either the homepage, or the adminpannel.
-	 */
 	@Override
 	public void beforeInvocation(Object o, Method m, Object[] params) throws Throwable
 	{
-		//We only need to check the user when the login-servlet is called
 		if (o instanceof Login)
-			System.out.println("(Login)About to Invoke --" + m.getName());
-				
+			System.out.println("-- (Login)About to Invoke --" + m.getName());				
 	}
 	
 	@Override
 	public void afterSuccess(Object o, Method m, Object[] params, Object result)
 	{
 		if (o instanceof Login)
-			System.out.println("(Login)SUCCES! Method -- " + m.getName() + " returned -- " + result);
+			System.out.println("-- (Login)SUCCES! Method -- " + m.getName() + " returned -- " + result);
 	}
 	
 	@Override
 	public void afterFailure(Object o, Method m, Object[] params, Throwable e)
 	{
 		if (o instanceof Login)
-			System.out.println("(Login)FAIL! Method -- " + m.getName() + " failed with -- " + e.getMessage());
+			System.out.println("-- (Login)FAIL! Method -- " + m.getName() + " failed with -- " + e.getMessage());
 	}
 	
 	@Override
 	public void after(Object o, Method m, Object[] params)
 	{
 		if (o instanceof Login)
-			System.out.println("(Login)After Method -- " + m.getName());
+			System.out.println("-- (Login)AFTER Method -- " + m.getName());
 	}
 }
