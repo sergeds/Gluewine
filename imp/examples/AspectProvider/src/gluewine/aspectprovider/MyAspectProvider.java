0edc2b1a0dc9fecb96a60eb0bc7200e40a07e901
GondaJanssen <gonda.janssen@fks.be>
2014-05-21 13:30:56 +0200

    Codeconventions AspectProvider example

--- imp/examples/AspectProvider/src/gluewine/aspectprovider/MyAspectProvider.java
+++ imp/examples/AspectProvider/src/gluewine/aspectprovider/MyAspectProvider.java
@@ -7,13 +7,16 @@ import org.gluewine.core.AspectProvider;
 public class MyAspectProvider implements AspectProvider
 {
 
+	//This method will run itself before each method that is executed
     @Override
     public void beforeInvocation(Object o, Method m, Object[] params) throws Throwable
     {
+    	//It will only print when a method of the class TestCommand is called on
         if (o instanceof TestCommand)
             System.out.println("--- About to invoke " + m.getName());
     }
 
+    //This method will run itself after a method is executed successfully
     @Override
     public void afterSuccess(Object o, Method m, Object[] params, Object result)
     {
@@ -21,6 +24,7 @@ public class MyAspectProvider implements AspectProvider
             System.out.println("--- Method " + m.getName() + " WAS SUCCESFULL AND RETURNED " + result);
     }
 
+    //This method will run itself after a method failed to execute
     @Override
     public void afterFailure(Object o, Method m, Object[] params, Throwable e)
     {
@@ -28,6 +32,8 @@ public class MyAspectProvider implements AspectProvider
             System.out.println("--- Method " + m.getName() + " FAILED with " + e.getMessage());
     }
 
+    //This method will run itself after a method is executed, 
+    //no matter whether the method failed or succeeded
     @Override
     public void after(Object o, Method m, Object[] params)
     {
