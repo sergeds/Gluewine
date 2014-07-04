9c6c6e6db32b296b67e4dc286162d3ac77cfebbd
Frank Gevaerts <frank.gevaerts@fks.be>
2014-07-04 12:03:36 +0200

    Add "gluedgwt", a gwt integratrion package that supports tight gluewine/gwt binding.

---
+++ imp/src/java/org/gluewine/gluedgwt/GluewineSerializationPolicy.java
@@ -0,0 +1,99 @@
+package org.gluewine.gluedgwt;
+
+import com.google.gwt.user.server.rpc.impl.TypeNameObfuscator;
+import com.google.gwt.user.server.rpc.impl.StandardSerializationPolicy;
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.server.rpc.SerializationPolicy;
+import java.util.Set;
+import java.util.HashMap;
+
+/**
+ * SerializationPolicy wrapper that supports type aliases.
+ *
+ * @author fks/Frank Gevaerts
+ */
+public class GluewineSerializationPolicy extends SerializationPolicy implements TypeNameObfuscator
+{
+    /** The parent policy. */
+    private StandardSerializationPolicy parent;
+
+    /** The alias table. */
+    private static HashMap<Class<?>, Class<?>> aliases = new HashMap<Class<?>, Class<?>>();
+
+    static
+    {
+        aliases.put(org.hibernate.collection.internal.PersistentSet.class, java.util.HashSet.class);
+        aliases.put(org.hibernate.collection.internal.PersistentList.class, java.util.ArrayList.class);
+        aliases.put(org.hibernate.collection.internal.PersistentMap.class, java.util.HashMap.class);
+        aliases.put(org.hibernate.collection.internal.PersistentSortedMap.class, java.util.TreeMap.class);
+        aliases.put(org.hibernate.collection.internal.PersistentSortedSet.class, java.util.TreeSet.class);
+    }
+
+    /**
+     * Constructs a GluewineSerializationPolicy.
+     * @param parent the parent to use.
+     */
+    public GluewineSerializationPolicy(StandardSerializationPolicy parent)
+    {
+        this.parent = parent;
+    }
+
+    /**
+     * Substitutes an alias if needed. TODO: add all hibernate types
+     * @param clazz the class to look at.
+     * @return the actual class to use.
+     */
+    private Class<?> substitute(Class<?> clazz)
+    {
+        Class<?> alias = aliases.get(clazz);
+        if (alias != null)
+        {
+            clazz = alias;
+        }
+        return clazz;
+    }
+
+    @Override
+    public final String getTypeIdForClass(Class<?> clazz) throws SerializationException
+    {
+        return parent.getTypeIdForClass(substitute(clazz));
+    }
+
+    @Override
+    public final String getClassNameForTypeId(String id) throws SerializationException
+    {
+        return parent.getClassNameForTypeId(id);
+    }
+
+
+    @Override
+    public void validateSerialize(Class<?> clazz) throws SerializationException
+    {
+        parent.validateSerialize(substitute(clazz));
+    }
+
+    @Override
+    public void validateDeserialize(Class<?> clazz) throws SerializationException
+    {
+        parent.validateDeserialize(substitute(clazz));
+    }
+
+    @Override
+    public boolean shouldSerializeFields(Class<?> clazz)
+    {
+        return parent.shouldSerializeFields(substitute(clazz));
+    }
+
+    @Override
+    public boolean shouldDeserializeFields(Class<?> clazz)
+    {
+        return parent.shouldDeserializeFields(substitute(clazz));
+    }
+
+    @Override
+    public Set<String> getClientFieldNamesForEnhancedClass(Class<?> clazz)
+    {
+        return parent.getClientFieldNamesForEnhancedClass(substitute(clazz));
+    }
+
+}
